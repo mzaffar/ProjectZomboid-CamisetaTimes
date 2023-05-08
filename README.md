10a0849a97b1bc91d3ced58145a9340355d505d8
Zaffar <zaffar@zaffar.com.br>
2023-05-08 15:16:19 -0300

    Inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+<h3 align="center">Project Zomboid - Camisetas de Times</h3>
+
+---
+
+<p align="center"> Adiciona ao jogo Project Zomboid camisetas dos times brasileiros de futebol.
+    <br> 
+</p>
+
+## 📝 Times atuais
+
+- Corinthans
+- Palmeiras
+- Santos
+- São Paulo
---
+++ TNTCamisetasTimes/media/lua/server/Items/TNTClothing_Distributions.lua
@@ -0,0 +1,56 @@
+require 'Items/SuburbsDistributions'
+require "Items/ProceduralDistributions"
+require "Vehicles/VehicleDistributions"
+require "Items/ItemPicker"
+
+-- TShirt Spawns Sã0 Paulo--
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_SaoPaulo");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_SaoPaulo");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_SaoPaulo_Time");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_SaoPaulo_Time");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+-- TShirt Spawns Corinthians--
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Corinthians");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Corinthians");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Corinthians_Time");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Corinthians_Time");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+-- TShirt Spawns Palmeiras--
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Palmeiras");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Palmeiras");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Palmeiras_Time");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Palmeiras_Time");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+-- TShirt Spawns Santos--
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Santos");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Santos");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
+
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, "TNT.Tshirt_Santos_Time");
+table.insert(ProceduralDistributions.list["ClothingStoresShirts"].items, 5);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "TNT.Tshirt_Santos_Time");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 5);
